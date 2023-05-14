8e2d1212ca4752bd60983e47d35ecb13de7300a3
penguinletofLv <406676015@qq.com>
2023-05-14 21:47:40 +0800

    hah

--- home_application/views.py
+++ home_application/views.py
@@ -13,7 +13,6 @@ specific language governing permissions and limitations under the License.
 
 from django.shortcuts import render
 
-
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
 def home(request):
